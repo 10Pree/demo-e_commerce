91c8a95aa9f76789ef45cdc5e05c4437041604fe
Nonthanan Simpree <nonthanansimpree@gmail.com>
2025-10-23 17:04:11 +0700

    feat(fronend) dashboard

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,10 +1,62 @@
-import Header from "@/components/dashboard/header"
-import SideBar from "@/components/dashboard/sidebar"
+import Header from "@/components/dashboard/header";
+import SideBar from "@/components/dashboard/sidebar";
+import Image from "next/image";
+export default function Page() {
+  return (
+    <div className="w-screen h-screen  flex flex-col">
+      <Header />
+      <div className="flex w-full h-full">
+        <SideBar />
+        <div className="w-full h-full flex justify-center items-center p-10">
+          <div className="w-full h-full">
+            <div className="flex gap-14 justify-center items-center">
+              <div className="w-[314px] h-[183px] bg-[#1E3A8A] flex justify-center items-center gap-20 rounded-3xl">
+                <Image
+                  className="pb-25 "
+                  src={"/icons/icons8-search.svg"}
+                  width={50}
+                  height={50}
+                  alt="icon"
+                />
+                <div className=" flex flex-col gap-5">
+                  <h1>รายได้วันนี้</h1>
+                  <h1>9999+</h1>
+                </div>
+              </div>
+              <div className="w-[314px] h-[183px] bg-[#1E3A8A] flex justify-center items-center gap-20 rounded-3xl">
+                <Image
+                  className="pb-25 "
+                  src={"/icons/icons8-search.svg"}
+                  width={50}
+                  height={50}
+                  alt="icon"
+                />
+                <div className=" flex flex-col gap-5">
+                  <h1>รายได้วันนี้</h1>
+                  <h1>9999+</h1>
+                </div>
+              </div>
+              <div className="w-[314px] h-[183px] bg-[#1E3A8A] flex justify-center items-center gap-20 rounded-3xl">
+                <Image
+                  className="pb-25 "
+                  src={"/icons/icons8-search.svg"}
+                  width={50}
+                  height={50}
+                  alt="icon"
+                />
+                <div className=" flex flex-col gap-5">
+                  <h1>รายได้วันนี้</h1>
+                  <h1>9999+</h1>
+                </div>
+              </div>
+            </div>
 
-export default function Page(){
-    return(
-        <div>
-            <SideBar/>
+            <div className="w-full h-4/5 bg-gray-200 ">
+                
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- frontend/components/dashboard/sidebar.js
+++ frontend/components/dashboard/sidebar.js
@@ -1,22 +1,30 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function SideBar() {
     const menus = [
-        { id:1, href: '/', label: "หน้าหลัก"},
-        { id:2, href: '/', label: "Dashboard"},
-        { id:3, href: '/', label: "ผู้ใช้งาน"},
-        { id:4, href: '/', label: "log"},
+        { id: 1, href: '/', label: "หน้าหลัก" },
+        { id: 2, href: '/', label: "Dashboard" },
+        { id: 3, href: '/', label: "ผู้ใช้งาน" },
+        { id: 4, href: '/', label: "Log" },
     ]
-    return(
-        <div className=" w-screen h-screen">
-            <nav className="bg-[#1E3A8A] w-[283px] h-full">
-                {menus.map((m) => {
-                    return(
-                        <div key={m.id}>
-                            <Link href={m.href}>{m.label}</Link>
-                        </div>
-                    )
-                })}
+    return (
+        <div className="w-[283px] h-full">
+            <nav className="bg-[#1E3A8A] h-full">
+                <div className=" flex flex-col gap-3 pl-10 pt-10">
+                    {menus.map((m) => {
+                        return (
+                            <div className="" key={m.id}>
+                                <Link className=" text-white" href={m.href}>{m.label}</Link>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="w-full pl-10 mt-20">
+                                    <Link className="cursor-pointer " href={"/"}>
+                    <Image src={"/icons/icons8-logout-50.png"} width={20} height={20} alt="Logo" />
+                </Link>
+                </div>
             </nav>
         </div>
     )
