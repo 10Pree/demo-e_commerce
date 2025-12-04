2b6327068f56f0c284afe4a32c2c960a808d93fe
Nonthanan Simpree <nonthanansimpree@gmail.com>
2025-12-04 15:56:13 +0700

    feat(frontend) page profile

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -1,17 +1,43 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
-    return(
-        <main className="w-full h-screen">
+    return (
+        <main className="w-full h-full mb-20 ">
             <div className=" flex justify-start items-center gap-2 ml-48 mt-12">
-                <div className="bg-[#1E3A8A] w-[25px] h-[25px] rounded-[8px] flex justify-center items-center">
+                <Link href={"/"} className="bg-[#1E3A8A] w-[25px] h-[25px] rounded-[8px] flex justify-center items-center">
                     <Image src={"/icons/icons8-back-w-52.png"} width={19} height={19} alt="icon" />
-                </div>
-                <span  className=" font-bold">กลับ</span>
+                </Link>
+                <span className=" font-bold text-[#111827]">กลับ</span>
             </div>
-            <div>
-                <div>
-
+            <div className=" flex justify-center items-center">
+                <div className="w-[646px] h-[720px] flex flex-col justify-center items-center bg-white shadow-xl/20 rounded-2xl border border-gray-400">
+                    <div className="w-[111px] h-[111px] bg-[#1E3A8A] flex justify-center items-center rounded-full">
+                        <Image src={"/icons/icons8-customer-96.png"} width={96} height={96} alt="icon" />
+                    </div>
+                    <div className="flex flex-col gap-4 justify-center items-center">
+                        <div>
+                            <span className="text-[20px] font-bold mb-4 text-[#111827]">ข้อมมูลส่วนตัว</span>
+                            <div className="bg-white shadow-xl border border-gray-400 rounded-[12px] w-[500px] h-[117px] text-[#929292] pl-4 pt-1">
+                                <span className="flex justify-end"><Link href={"/"} className="text-[#FACC15] mr-4">แก้ไข</Link></span>
+                                <p>ชื่อ : ................................................................................</p>
+                                <p>ชื่อ : ................................................................................</p>
+                                <p>ชื่อ : ................................................................................</p>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="text-[20px] font-bold text-[#111827]">ที่อยู่</span>
+                            <div className="bg-white shadow-xl border border-gray-400 rounded-[12px] w-[500px] h-[117px] text-[#929292] pl-4 pt-1">
+                                <span className="flex justify-end"><Link href={"/"} className="text-[#FACC15] mr-4">แก้ไข</Link></span>
+                                <p>ชื่อ : ................................................................................</p>
+                            </div>
+                        </div>
+                        <div className="w-[500px] flex justify-start">
+                            <Link href={"/"} className="bg-white shadow-xl border border-gray-400 rounded-[12px] w-[233px] h-[53px] text-[20px] font-bold flex justify-center items-center text-[#111827]">
+                                ดูประวัติคำสั่งซื้อของฉัน
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
