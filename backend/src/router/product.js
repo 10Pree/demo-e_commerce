67361da28cfb43fe09585e5a70ff7b799c3aa74c
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-01-16 16:56:11 +0700

    feat(backend) add Authorize to router product

--- backend/src/router/product.js
+++ backend/src/router/product.js
@@ -1,14 +1,15 @@
 const express = require('express')
+const Authorize = require('../middlewares/oAuth')
 const controllerProduct = require('../controllers/product')
 
 const router = express.Router()
 // router.use(express.json())
 
-router.post('/product', controllerProduct.Create)
+router.post('/product',Authorize("project.create"), controllerProduct.Create)
 router.get('/products', controllerProduct.Reads)
-router.get('/product/:id', controllerProduct.Read)
+router.get('/product/:id',Authorize("project.read"), controllerProduct.Read)
 router.get('/product/code/:code', controllerProduct.ReadCode)
-router.put('/product/:id', controllerProduct.Update)
-router.delete('/product/:id', controllerProduct.Delete)
+router.put('/product/:id',Authorize("project.edit"), controllerProduct.Update)
+router.delete('/product/:id',Authorize("product.delete"), controllerProduct.Delete)
 
 module.exports = router
