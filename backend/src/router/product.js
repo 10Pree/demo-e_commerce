0dd029b4ea4522ba9f000c1a820ad3235976c26e
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-01-30 16:25:27 +0700

    fix router

--- backend/src/router/product.js
+++ backend/src/router/product.js
@@ -10,6 +10,6 @@ router.get('/products', controllerProduct.Reads)
 router.get('/product/:id', controllerProduct.Read)
 router.get('/product/code/:code', controllerProduct.ReadCode)
 router.put('/product/:id',Authorize("project.edit"), controllerProduct.Update)
-router.delete('/product/:id',Authorize("product.delete"), controllerProduct.Delete)
+router.delete('/product/:id',Authorize("project.delete"), controllerProduct.Delete)
 
 module.exports = router
