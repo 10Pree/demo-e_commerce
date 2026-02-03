ec1336cfe1d05c9e7e1e2a45be6f6644831422be
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-02-03 23:29:19 +0700

    feat(backend) use multer

--- backend/src/services/uplode.js
+++ backend/src/services/uplode.js
@@ -1,7 +1,24 @@
 
 const multer = require('multer')
-const upload = multer({ dest: '/uploads'})
+const path = require('path')
+
+const path_Products = '/uploads/products'
 
 const storage = multer.diskStorage({
-    
+    destination: (req, File, cd) => {
+        cd(null, path_Products)
+    },
+    filename: (req, File, cd) => {
+        const ext = path.extname(File.originalname)
+        const name = Date.now() + "-" + Math.round(Math.random() * 1E9)
+        cd(null, name + ext)
+    }
+
 })
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024}
+})
+
+module.exports = upload
