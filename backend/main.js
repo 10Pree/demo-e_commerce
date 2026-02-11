c0667166b15af5ebf1ed3632b9944aad33ff57f5
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-02-11 11:12:39 +0700

    fix(backend) fix cannot upload images

--- backend/main.js
+++ backend/main.js
@@ -19,11 +19,9 @@ app.use(cors({
      origin: "http://localhost:3000",
      credentials: true
 }))
-app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true}))
-
-
+app.use(express.json());
 
 
 app.use('/', products)
--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -10,6 +10,7 @@ class controllerProduct {
         try {
             const { p_name, p_price, p_details, p_stock, categories_ids } = req.body || {};
             const image_url = req.files
+            console.log("CONTENT-TYPE:", req.headers['content-type']);
             console.log("FILES:", req.files);
             console.log("BODY:", req.body);
 
