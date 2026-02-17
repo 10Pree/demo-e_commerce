c4eb5f0e93e61f717e67b205548bbc2c6608e4c5
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-02-17 14:32:40 +0700

    fix(backend) fix name upload to uploadProduct

--- backend/src/router/product.js
+++ backend/src/router/product.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const Authorize = require('../middlewares/oAuth')
 const controllerProduct = require('../controllers/product')
-const upload = require('../services/upload')
+const { uploadProduct } = require('../services/upload')
 
 const router = express.Router()
 // router.use(express.json())
 
-router.post('/product', Authorize("project.create"), upload.array('images', 12),controllerProduct.Create);
+router.post('/product', Authorize("project.create"), uploadProduct.array('images', 12),controllerProduct.Create);
 router.get('/products', controllerProduct.Reads)
 router.get('/product/:id', controllerProduct.Read)
 router.get('/product/code/:code', controllerProduct.ReadCode)
-router.put('/product/:id',Authorize("project.edit"), upload.array('images', 12), controllerProduct.Update)
+router.put('/product/:id',Authorize("project.edit"), uploadProduct.array('images', 12), controllerProduct.Update)
 router.delete('/product/:id',Authorize("project.delete"), controllerProduct.Delete)
 
 module.exports = router
