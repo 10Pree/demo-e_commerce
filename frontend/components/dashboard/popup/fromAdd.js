315a8d2db43e90e312795aed1f18782218066ef9
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-03-18 17:27:06 +0700

    feat(frontend) make popup add user

---
+++ frontend/components/dashboard/popup/fromAdd.js
@@ -0,0 +1,27 @@
+export default function fromAdd({popup}) {
+    return (
+        <div className=" z-99 w-full h-full bg-black/60 fixed inset-0 flex justify-center items-center">
+            <div className="bg-white md:w-[30%] w-[90%] md:h-[40%] h-[45%] rounded-2xl">
+                <div className="px-3 py-4 flex justify-end">
+                    <img src="/icons/icons8-forward-52.png" width={30} height={30} onClick={}/>
+                </div>
+                <div className="w-full h-full flex flex-col items-center">
+                    <h1 className="text-center font-bold">เพิ่มผู้ใช้งาน</h1>
+                    <div className="w-full flex flex-col justify-center items-center gap-4">
+                        <div className="w-[50%] flex flex-col gap-2">
+                            <h2>รหัสผ่าน</h2>
+                            <input className="w-full h-10 p-3 border-2 rounded-xl" type="password" placeholder="รหัสผ่าน"/>
+                        </div>
+                        <div className="w-[50%] flex flex-col gap-2">
+                            <h2>รหัสผ่าน อีกครั้ง</h2>
+                            <input className="w-full h-10 p-3 border-2 rounded-xl" type="password" placeholder="รหัสผ่านอีกครั้ง"/>
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center mt-4">
+                        <button className="bg-[#1E3A8A] py-3 px-4 rounded-xl text-white">ยืนยัน</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
