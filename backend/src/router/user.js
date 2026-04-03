5345d64a953944644d2fcd455e3845b87d325c03
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-04-03 17:39:29 +0700

    feat(backend) Add authorization to user read endpoints
    Require authorization on user read routes: apply Authorize('project.read') to GET /users and Authorize('project.edit') to GET /user/:id to enforce permission checks. Also normalize spacing on the PUT /user/:id route.

--- backend/src/router/user.js
+++ backend/src/router/user.js
@@ -7,9 +7,9 @@ const router = express.Router()
 router.use(express.json())
 
 router.post('/user', Authorize("project.create"), uploadUser.array('images', 1), controllersUser.Create)
-router.get('/users', controllersUser.Reads)
-router.get('/user/:id', controllersUser.ReadById)
-router.put('/user/:id',Authorize("project.edit"), uploadUser.array('images', 1), controllersUser.Update)
+router.get('/users', Authorize("project.read"), controllersUser.Reads)
+router.get('/user/:id', Authorize("project.edit"), controllersUser.ReadById)
+router.put('/user/:id', Authorize("project.edit"), uploadUser.array('images', 1), controllersUser.Update)
 router.put('/user/password/:id', Authorize("project.edit"), controllersUser.UpdatePassword)
 router.delete('/user/:id', Authorize("project.delete"), controllersUser.Delete)
 
