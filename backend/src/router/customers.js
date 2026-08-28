9201d297682a819efbedfe6a521c388d7380ae1b
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-08-28 18:08:54 +0700

    fix(backend) Fix url router upload image

--- backend/src/router/customers.js
+++ backend/src/router/customers.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.post('/customer', uploadCustomers.array('images', 1), controllerCustomers.Create)
 router.get('/customers', controllerCustomers.GetCustomers)
 router.get('/customer/:id', controllerCustomers.GetCustomerById)
-router.put('/customer/:id', uploadCustomers.single('images') , controllerCustomers.UpdateCustomer)
+router.put('/customer/:id', uploadCustomers.array('images', 1) , controllerCustomers.UpdateCustomer)
 router.put('/customer/updatepassword/:id', uploadCustomers.single('images'), controllerCustomers.UpdatePassword)
 router.delete('/customer/:id', controllerCustomers.softdelete)
 
